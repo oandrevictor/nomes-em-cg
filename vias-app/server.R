75c3be1623e8c83b7d7ec8a6f680f292dadd96ec
André Lopes <oandrevictor@gmail.com>
2018-06-12 23:16:28 -0300

    Develop app

--- vias-app/server.R
+++ vias-app/server.R
@@ -11,33 +11,80 @@ library(shiny)
 library(tidyverse)
 library(here)
 source(here("code/read_wrangle.R"))
+library(ggplot2)
+library(plotly)
+library(DT)
+
 
 vias = read_wrangle_data()
 
 profissoes_nos_dados = vias %>% 
-  filter(!is.na(profissao)) %>%  
-  pull(profissao) %>% 
-  unique()
+    filter(!is.na(profissao)) %>%  
+    pull(profissao) %>% 
+    unique()
+
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     prof_selecionada = reactive({input$profissao})
+    minlen = reactive({input$minlen[1]})
+    maxlen =  reactive({input$minlen[2]})
     
-    output$distPlot <- renderPlot({
-        vias_profissao = vias %>% filter(profissao == prof_selecionada())
-        vias_profissao %>% 
-            ggplot(aes(x = arvores_100m_mean)) + 
-            geom_histogram(binwidth = 1, 
-                           boundary = 0, 
-                           fill = "darkgreen") + 
-            scale_x_continuous(limits = c(0, 15))
+    output$distPlot <- renderPlotly({
+        vias_profissao = vias %>%
+            filter(profissao == prof_selecionada(), 
+                   comprimento >= minlen(),
+                   comprimento <= maxlen())
+        
+        plot_profissoes = vias_profissao %>%
+            ggplot(aes(x=logradouro,
+                       y=comprimento,
+                       fill = as.factor(profissao),
+                       text = paste(nomelograd, " - ",
+                                    comprimento,"m"))) +
+            geom_point() +
+            theme(axis.text.x = element_text(angle = 90,
+                                             hjust = 1),
+                  legend.position="none") + 
+            labs(x = "Logradouro", y = "Comprimento (m)")
+        
+        ggplotly(plot_profissoes, tooltip = c("text")) %>%
+            layout(margin = list(b = 100))
+        })
+    
+    # PLOTS
+    output$distViasProf <- renderPlotly({
+        vias_profissao_validas = vias %>% filter(!is.na(profissao),
+                                                 comprimento >= minlen(),
+                                                 comprimento <= maxlen())
+        plot = vias_profissao_validas %>% 
+            ggplot(aes(x = profissao),
+                   stat=count) +
+            geom_bar(aes(x = profissao,
+                         fill = as.factor(profissao),
+                         text = paste(profissao),
+                         label = ..count..), 
+                     position = "dodge",
+                     stat = "count"
+                     )  +
+            theme(axis.text.x = element_text(angle = 90,
+                                             hjust = 1),
+                  legend.position="none") + 
+            labs(x = "Profissão", y = "Total")
+        
+        ggplotly(plot, tooltip = c("text","label")) %>%
+            layout(margin = list(b = 100))
+        
     })
     
-    output$listagem <- renderTable({
-        vias %>% 
-            filter(profissao == prof_selecionada()) %>% 
-            select(nome = nomelograd, 
-                   comprimento)
-    })
+      output$ex1 <- DT::renderDataTable(
+        DT::datatable( vias %>%
+                           filter(!is.na(profissao)) %>%
+                           filter(comprimento >= minlen(),
+                                  comprimento <= maxlen()) %>%
+                           select(logradouro,nomelograd,comprimento),
+                       options = list(pageLength = 10))
+    )
     
 })
--- vias-app/ui.R
+++ vias-app/ui.R
@@ -2,6 +2,9 @@ library(shiny)
 library(tidyverse)
 library(here)
 source(here("code/read_wrangle.R"))
+library(DT)
+library(plotly)
+
 
 profissoes_nos_dados = read_wrangle_data() %>% 
     filter(!is.na(profissao)) %>%  
@@ -11,27 +14,26 @@ profissoes_nos_dados = read_wrangle_data() %>%
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Ruas mais arborizadas por profissão"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-        selectInput("profissao", 
-                    "Profissão", 
-                    choices = profissoes_nos_dados), 
-        sliderInput("bins",
-                    "Number of bins:",
-                    min = 1,
-                    max = 50,
-                    value = 30)
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot"), 
-       tableOutput("listagem")
+    # Application title
+    titlePanel("Nome das ruas por profissão"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("minlen", "Tamanho mínimo e máximo da via:", value = c(0,20000), min = 1, max = 20000),
+            selectInput("profissao", 
+                        "Profissão", 
+                        choices = profissoes_nos_dados) 
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            titlePanel("Quantidade de vias por profissão"),
+            plotlyOutput("distViasProf"),
+            titlePanel("Vias para a profissão escolhida"),
+            plotlyOutput("distPlot"), 
+            DT::dataTableOutput('ex1')
+        )
     )
-  )
 ))
